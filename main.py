a893faec70d32d9c61cae94e98c37fce18d9e4af
Carson Szeder <carsonszeder@gmail.com>
2024-02-10 05:32:16 -0800

    port

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def testEndpoint():
       return{'Test Status': 'Successful'}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
